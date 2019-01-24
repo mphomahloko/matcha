d18847f44f28976ee9f566b6dde0b57d82c97a04
Mpho MAHLOKO <mmahloko@e4r9p4.wethinkcode.co.za>
2019-01-24 16:51:25 +0200

    Login

---
+++ app/login.php
@@ -0,0 +1,13 @@
+<?php include_once('../public/default.php'); ?>
+
+<div class="container">
+    <h2 class="text-center ">Login</h2>
+    <form action="" method="post">
+        <div class="form-group">
+            <input type="text" name="username" id="user" class="form-control" placeholder="Username">
+        </div>
+        <div class="form-group">
+            <input type="text" name="pwd" id="pwd" class="form-control" placeholder="Password">
+        </div>
+    </form>
+</div>
---
+++ index.php
@@ -0,0 +1,2 @@
+<?php include_once('public/default.php'); ?>
+<h1 class="text-center">Landing Page</h1>
