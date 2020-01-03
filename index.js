d688caea14d323ae23ec2e0de8a71c312bf6ffad
Mpho MAHLOKO <mmahloko@c5r9s5.wethinkcode.co.za>
2020-01-03 12:53:38 +0200

    adding routes

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.get('/', (req, res)=>{
     res.send("Hello World!");
+});
+
+app.get('/login', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/login.html'));
+});
+
+app.get('/register', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/register.html'));
 });
 
 app.listen(3000);
