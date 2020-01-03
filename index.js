1b54188400273ac9c58a7aebec7806f1a4752c2f
Mpho MAHLOKO <mmahloko@c5r9s5.wethinkcode.co.za>
2020-01-03 17:09:54 +0200

    babel setup

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+import express from 'express';
+import path from 'path';
+import open from 'open';
+
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res)=>{
-    res.send("Hello World!");
+    res.sendFile(path.join(__dirname, './src/index.html'));
 });
 
 app.get('/login', (req, res)=>{
-    res.sendFile(path.join(__dirname, '/login.html'));
+    res.sendFile(path.join(__dirname, './src/login.html'));
 });
 
 app.get('/register', (req, res)=>{
-    res.sendFile(path.join(__dirname, '/register.html'));
+    res.sendFile(path.join(__dirname, './src/register.html'));
 });
 
-app.listen(3000);
+app.get('*', (req, res)=>{
+    // res.render('login');
+    res.send("404 Page Not Found");
+});
+
+app.listen(port, (err)=>{
+    if (err){
+        console.log(err);
+    } else {
+        open('http://localhost:' + port);
+    }
+});
