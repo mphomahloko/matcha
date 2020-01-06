1079c2fe88f1cd2afbf984882b498ead5cb09308
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2020-01-06 18:56:00 +0200

    insert data

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import open from 'open';
+// import open from 'open';
 import app from './app';
 
-const port = 3000;
+const port = 4000;
 
 const server = app.listen(port, (err)=>{
     if (err){
         console.log(err);
     } else {
-        open('http://localhost:' + port);
+        // open('http://localhost:' + port);
         console.log(`Express is running on port ${server.address().port}`);
     }
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -48,6 +48,60 @@ router.get('/register', (req, res)=>{
     res.render('pages/register');
 });
 
+router.post('/register', (req, res)=>{
+
+    let user = req.body.username;
+    let pass = req.body.password;
+    let email = req.body.email;
+    if (user && pass && email)
+    {
+        let validUserPattern = /(?=^.{2,50}$)(?=.*[a-z])(?=.*[A-Z]).*$/;
+        let validPassPattern = /(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$/;
+        let validEmailPattern = /[\w-]+@([\w-]+\.)+[\w-]+/;
+
+        let validate_user = user.match(validUserPattern);
+        let validate_pass = pass.match(validPassPattern);
+        let validate_email = email.match(validEmailPattern);
+
+        if (!validate_user || !validate_email || !validate_pass)
+        {
+            if (!validate_user)
+            {
+                console.log("your username need to be 2 - 50 characters long and contain atleast lower and uppercase alphabets");
+            }
+
+            if (!validate_email)
+            {
+                console.log("your email needs to be in this format: user@mail.domain");
+            }
+
+            if (!validate_pass)
+            {
+                console.log("a password must contain lower and upper case characters, digit(s), and special character(s)");
+            }
+        }
+
+        else
+        {
+            db.query('INSERT INTO matcha_users (username, password, email) VALUES (?,?,?)', [user, pass, email], (err, results) => {
+                if (results.affectedRows)
+                {
+                    console.log("user info succesfully inserted into database");
+                }
+                else if (err)
+                {
+                    console.log("failed to insert to database");
+                }
+            });
+        }
+    }
+    else
+    {
+        console.log("all fields need to be filled in");
+    }
+    res.send(pass);
+});
+
 
 router.get('*', (req, res)=>{
      res.render('pages/index');
