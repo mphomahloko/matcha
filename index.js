1be9ad006ce888d5d9b3bec2b0c03ebeb98b6c4a
Mpho <43447189+Mahloko@users.noreply.github.com>
2020-01-30 10:33:38 +0200

    dummy chat

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.post('/messages', (req, res)=>{
 	// insert msg into database accordingly
     console.log(req.body);
     // send msg to specific user
-	io.emit(req.session.username, req.body);
+	io.emit(req.body.to, req.body);
 	res.status(200).send({success: true});
 });
 
@@ -17,7 +17,7 @@ io.on('connection', () =>{
 });
 
 const server = http.listen(port, (err)=>{
-    if (err){
+    if (err) {
         console.log(err);
     } else {
         console.log(`Express is running on port ${server.address().port}`);
--- public/js/custom/messages.js
+++ public/js/custom/messages.js
@@ -11,13 +11,22 @@ const sendMsg = () => {
 	let msg = document.querySelector(".write_msg").value;
 	if (msg.trim()) {
 		const api = 'http://localhost:3000/messages';
+		let sender = document.querySelector('._').value;
+		let reciever = "";
+		if (sender == "santa")
+			reciever = "test";
+		else
+			reciever = "santa";
 
 		fetch(api, {
 			method: 'POST',
 			headers: {
 			  'Content-Type': 'application/json;charset=utf-8'
 			},
-			body: JSON.stringify({message: msg.trim()})
+			body: JSON.stringify({
+				message: msg.trim(),
+				to: reciever
+			})
 		}).then((res) => { return res.json(); })
           .then()
 		  .catch((error) => { console.error('Error', error);});
