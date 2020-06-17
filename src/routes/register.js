92227d73d0bb042a02f5e87a90db18b8fb6220ca
jphasha <50695096+jphasha@users.noreply.github.com>
2020-06-17 18:31:25 +0200

    Feature/send confirmation email (#20)
    * the registration seems to work. The problem I have is that the app will register the user even if the registration email was not sent .e.g. if the was no internet and thus the app could not send the email. the email sending needs restructuring and for now i am drawing a blank
    
    * this is just some patch work, I am not sure that this is even allowed
    
    * patch on user not recieving an email...
    
    * the no network problem is quiet on my side
    
    Co-authored-by: Mpho Mahloko

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,21 +1,29 @@
 import express from 'express';
 import db from '../../config/database/database';
 import passEncrypt from 'bcryptjs';
-import validators from '../models/validators';
+import validators from '../utils/validators';
+import { regToken } from '../utils/registrationToken';
+import nodemailer from 'nodemailer';
+require('dotenv').config();
 
 const router = express.Router();
 
 // /register routes
 router.get('/', (req, res) => {
     if (req.session.loggedin) {
-        res.render('pages/home', { username: req.session.username });
+        res.render('pages/home', {
+            username: req.session.username
+        });
     } else {
-        res.render('pages/register', { success: true, message: "Complete form to register" });
+        res.render('pages/register', {
+            success: true,
+            message: 'Complete form to register'
+        });
     }
     res.end();
 });
 
-router.post('/',  (req, res) => {
+router.post('/', (req, res) => {
 
     let user = req.body.username;
     let pass = req.body.password;
@@ -26,48 +34,93 @@ router.post('/',  (req, res) => {
 
     if (validators.validateUsername(user) && validators.validatePass(pass) &&
         validators.validateEmail(email) && validators.validateConfPass(pass, confPass) &&
-        validators.validateLastName(firstName) && validators.validateFirstName(lastName)) {
+        validators.validateLastName(lastName) && validators.validateFirstName(firstName)) {
 
         passEncrypt.hash(pass, 8, (err, hashedPass) => {
             if (err) {
                 return err;
             }
             db.query('SELECT username FROM matcha_users WHERE username=?', // unique username
-                [user], (err, results, field) => {
+                [user], (err, results) => {
                     if (err) return err;
 
                     if (results.length == 0) {
                         db.query('SELECT email FROM matcha_users WHERE email=?', // unique email
-                            [email], (err, results, field) => {
-                                if (err) return err;
-                                if (results.length == 0) {
-                                    // ready for insert statement
-                                    db.query('INSERT INTO matcha_users (password, username, email, active, firstname, lastname) VALUES (?, ?, ?, ?, ?, ?)',
-                                        [hashedPass, user, email, 0, firstName, lastName],
-                                        async (err, results, field) => {
-                                        if (results) {
-
-                                            res.status(200).render('pages/login', {
-                                                success: true,
-                                                message: "successfully registered, please click on the link in your email to activate your account"
+                            [email],
+                            (err, results) => {
+                            if (err) return err;
+                            if (results.length == 0) {
+                                // ready for insert statement
+                                let token = regToken(20);
+                                db.query('INSERT INTO matcha_users (password, username, email, active, firstname, lastname, token) VALUES (?, ?, ?, ?, ?, ?, ?)',
+                                    [hashedPass, user, email, 0, firstName, lastName, token],
+                                    async (err, results) => {
+                                    if (results) {
+                                        let transporter = nodemailer.createTransport({
+                                            service: 'gmail',
+                                            auth: {
+                                                user: process.env.MATCHA_EMAIL,
+                                                pass: process.env.EMAIL_PASS
+                                            },
+                                            tls: {
+                                                rejectUnauthorized: false
+                                            }
+                                        });
+                                        
+                                        let mailOptions = {
+                                            from: process.env.MATCHA_EMAIL,
+                                            to: email,
+                                            subject: 'Welcome to Matcha',
+                                            html: `<p>Congradulations, you have started on the right journey to find your true love with online dating</p>
+                                                   <p>Please click on this <a href="http://localhost:4000/login?user=${user}&token=${token}">link</a>
+                                                   to activate your account</p>`
+                                        };
+                                        
+                                        transporter.sendMail(mailOptions, (err) => {
+                                            if (err) {
+                                                res.status(401).render('pages/register', {
+                                                    success: false,
+                                                    message: 'failed to register you for some reason. please try registering again'
+                                                });
+                                                res.end();
+                                                console.log(err);
+                                                db.query('DELETE FROM matcha_users WHERE email=?', [email], (erRor) => {
+                                                if (erRor) {
+                                                    console.log("failed to remove user from database");
+                                                } else {
+                                                    console.log("user removed from database");
+                                                }
                                             });
-                                            res.end();
-                                        }
-                                        else {
-                                            res.status(401).render('pages/register', {
-                                                success: false,
-                                                message: "failed to register you for some reason. please try registering again"
-                                            });
-                                            res.end();
-                                            console.log(err);
-                                        }
-                                    });
-                                } else {
-                                    res.status(401).render('pages/register', { success: false, message: "email already exists" });
-                                }
-                            });
+                                            } else {
+                                                console.log(`Email sent to: ${email}`);
+                                                res.status(200).render('pages/login', {
+                                                    success: true,
+                                                    message: 'successfully registered, please click on the link in your email to activate your account.'
+                                                });
+                                                res.end();
+                                            }
+                                        });
+                                    } else {
+                                        res.status(401).render('pages/register', {
+                                            success: false,
+                                            message: 'failed to register you for some reason. please try registering again'
+                                        });
+                                        res.end();
+                                        console.log(err);
+                                    }
+                                });
+                            } else {
+                                res.status(401).render('pages/register', {
+                                    success: false,
+                                    message: 'email already exists'
+                                });
+                            }
+                        });
                     } else {
-                        res.status(401).render('pages/register', { success: false, message: "user already exists" });
+                        res.status(401).render('pages/register', {
+                            success: false,
+                            message: 'user already exists'
+                        });
                     }
                 });
         });
@@ -75,29 +128,26 @@ router.post('/',  (req, res) => {
         if (!validators.validateUsername(user)) {
             res.status(401).render('pages/register', {
                 success: false,
-                message: "your username need to be 2 - 50 characters long and contain at least one lower case alphabet"
+                message: 'your username need to be 2 - 50 characters long and contain at least one lower case alphabet'
             });
             res.end();
-        }
-        else if (!validators.validateEmail(email)) {
-            res.status(401).render('pages/register', {
-                success: false, message:
-                    "your email needs to be in this format: user@mail.domain"
-            });
-            res.end();
-        }
-        else if (!validators.validatePass(pass)) {
-            console.log("a password must contain lower and upper case characters, digit(s), and special character(s)");
+        } else if (!validators.validateEmail(email)) {
             res.status(401).render('pages/register', {
                 success: false,
-                message: "a password must contain lower and upper case characters, digit(s), and special character(s)"
+                message: 'your email needs to be in this format: user@mail.domain'
             });
             res.end();
-        }
-        else if (!validators.validateConfPassword(pass, confPass)) {
+        } else if (!validators.validatePass(pass)) {
+            console.log('a password must contain lower and upper case characters, digit(s), and special character(s)');
             res.status(401).render('pages/register', {
                 success: false,
-                message: "your confirm password must match your password"
+                message: 'a password must contain lower and upper case characters, digit(s), and special character(s)'
+            });
+            res.end();
+        } else if (!validators.validateConfPass(pass, confPass)) {
+            res.status(401).render('pages/register', {
+                success: false,
+                message: 'your confirm password must match your password'
             });
             res.end();
         }
