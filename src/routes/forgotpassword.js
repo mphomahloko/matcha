932811fd6c119f00c29e706ce42711f71ad663d3
jphasha <jphasha@student.wethinkcode.co.za>
2020-06-27 20:54:38 +0200

    i am missing something on the reset password

--- src/routes/forgotpassword.js
+++ src/routes/forgotpassword.js
@@ -22,21 +22,22 @@ forgotPasswordRoute.post('/', async (req, res) => {
             const auth = new Auth;
             user.forgotPassword(email);
             await auth.forgotPassword(user);
+            
+        } catch (err) {
+            console.log(err);
+        } finally {
             res.status(200).render('pages/login', {
                 success: true,
                 message: 'check your email'
             });
-        } catch (err) {
-            res.status(401).render('pages/login', {
-                success: false,
-                message: 'check your email'
-            });
+            res.end();
         }
     } else {
         res.status(401).render('pages/forgotpassword', {
             success: false,
             message: 'invalid email format, an email must resemble: "email@host.domain"'
         });
+        res.end();
     }
 });
 
--- src/routes/resetpassword.js
+++ src/routes/resetpassword.js
@@ -1,13 +1,46 @@
 import express from 'express';
 
+import validators from '../utils/validators';
+
 const resetPasswordRoute = express.Router();
 
 resetPasswordRoute.get('/', async (req, res) => {
-    res.status(200).render('pages/resetpassword', {
-        success: true,
-        message: 'Please enter and confirm your new password'
-    })
-    res.end();
+    const linkUser = req.query.user;
+    const linkToken = req.query.token;
+    if (linkUser && linkToken)
+    {
+        res.status(200).render('pages/resetpassword', {
+            success: true,
+            message: 'Please enter and confirm your new password'
+        });
+        res.end();
+    } else {
+        res.status(200).render('pages/login', {
+            success: true,
+            message: 'Have an account? login'
+        });
+        res.end();
+    }
+});
+
+resetPasswordRoute.post('/', async (req, res) => {
+    const password = req.body.password;
+    const confPass = req.body.confpass;
+    if (validators.validatePass(password) && validators.validateConfPass(password, confPass)) {
+        console.log('yes');
+        res.status(200).render('pages/login', {
+            success: true,
+            message: 'let us go'
+        });
+        res.end();
+    } else if (!validators.validatePass(password) || !validators.validateConfPass(password, confPass)) {
+        console.log('no');
+        res.status(401).render('pages/resetpassword', {
+            success: false,
+            message: 'Please enter and confirm your new password'
+        });
+        res.end();
+    }
 });
 
 module.exports = resetPasswordRoute;
