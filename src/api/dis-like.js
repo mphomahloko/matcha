94dd5bee5f079c6992edc695db6a3231b2382987
Mpho <43447189+Mahloko@users.noreply.github.com>
2020-07-09 08:24:28 +0200

    use promises

--- src/api/dis-like.js
+++ src/api/dis-like.js
@@ -1,75 +1,60 @@
 import express from 'express';
-import db from "../../config/database/connection";
+import query from '../utils/dbqueries'
 
 const router = express.Router();
 
 router.route('/')
-    .get((req, res) => {
-        db.query('SELECT * FROM matcha.likes WHERE participant=? AND liked_participant=?',
-        [req.query.participant, req.query.liked_participant],
-            (err, results) => {
-                if (results[0]) {
-                    res.status(200).json({
-                        success: true,
-                        message: "dislike"
-                    });
-                } else {
-                    res.status(200).json({
-                        success: true,
-                        message: "like"
-                    });
-                }
-            })
-    })
+  .get(async (req, res) => {
+    try {
+      const isLiked = await query.isUserLiked(req.query.participant, req.query.liked_participant)
+      if (isLiked) {
+        res.status(200).json({
+          success: true,
+          message: "dislike"
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          message: "like"
+        })
+      }
+    } catch (ex) {
+      console.log("an error occured: ", ex.message);
+    }
+  });
 
-    
 router.route('/like')
-.post((req, res) => {
-    db.query('INSERT INTO matcha.likes (participant, liked_participant) VALUES (?, ?)',
-        [req.body.participant, req.body.liked_participant],
-        (err, results) => {
-        if (results) {
-            // check to see if users liked each other
-            db.query(`INSERT INTO matcha.rooms (participant_1, participant_2)
-                            SELECT participant, liked_participant
-                            FROM likes
-                            WHERE liked_participant LIKE ?
-                            AND participant LIKE ?;`,
-                [req.body.participant, req.body.liked_participant],
-                (err, results) => {
-                res.status(200).json({
-                  success: true,
-                  message: "like successful ..."
-                });
-            });
-        }
-        else {
-            res.status(400).json({
-              success: false,
-              message: "failed to like..."
-            });
-            console.log(err);
-        }
-    });
-})
+  .post(async (req, res) => {
+    try {
+      await query.likeUser(req.body.participant, req.body.liked_participant);
+      await query.userLikedBack(req.body.participant, req.body.liked_participant);
+      res.status(200).json({
+        success: true,
+        message: "like successful ..."
+      });
+    } catch (ex) {
+      console.log(ex.message);
+      res.status(200).json({
+        success: false,
+        message: "failed to like..."
+      });
+    }
+  })
 
 router.route('/dislike')
-    .post((req, res) => {
-        db.query('DELETE FROM matcha.likes WHERE participant=? AND liked_participant=?',
-        [req.body.participant, req.body.liked_participant],
-        (error, results) => {
-            if (error) {
-                res.status(200).json({
-                    success: false,
-                    message: "Failed to dislike user, please try again later..."
-                });
-            } else {
-                res.status(200).json({
-                    success: true,
-                    message: "disliked user successfully ..."
-                });
-            }
-        })
-    })
+  .post(async (req, res) => {
+    try {
+      await query.disLike(req.body.participant, req.body.liked_participant)
+      res.status(200).json({
+        success: true,
+        message: "disliked user successfully ..."
+      });
+    } catch (error) {
+      res.status(200).json({
+        success: false,
+        message: "Failed to dislike user, please try again later..."
+      });
+    }
+  })
 
 module.exports = router;
