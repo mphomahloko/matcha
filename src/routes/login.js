9326906d818870920c4e25c3fe278a6c61c37312
Mpho <43447189+Mahloko@users.noreply.github.com>
2020-07-09 19:07:42 +0200

    login fixes

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import _ from 'lodash';
 
 import Auth from '../controller/auth';
 import User from '../models/user'
 import validators from '../utils/validators';
+import query from '../utils/dbqueries'
 
 const loginRoute = express.Router();
 const auth = new Auth();
@@ -11,10 +13,25 @@ const auth = new Auth();
 loginRoute.route('/')
   .get(async (req, res) => {
     if (req.session.loggedin) {
-      res.status(200).render('pages/home', {
-        username: req.session.username,
-        users: []
-      });
+      try {
+        let user = req.session.username;
+        let suggestedUsers = await query.getSuggestedUsers();
+        if (suggestedUsers.length > 0) {
+          res.status(200).render('pages/home', {
+            username: user,
+            users: _.filter(suggestedUsers, (suggestedUser => {
+              return suggestedUser.username.localeCompare(user);
+            }))
+          })
+        } else {
+          res.status(200).render('pages/home', {
+            username: user,
+            users: []
+          })
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
     }
     else {
       const linkName = req.query.user;
@@ -52,23 +69,33 @@ loginRoute.route('/')
     if (validators.validateUsername(username) && validators.validatePass(pass)) {
       try {
         let user = new User();
-        user.login({username: username, password: pass})
+        let suggestedUsers = await query.getSuggestedUsers();
+        user.login({ username: username, password: pass })
         user = await auth.login(user);
         req.session.loggedin = true;
         req.session.username = user.username;
         req.session.user_id = user.user_id;
 
-        res.status(200).render('pages/home', {
-          username: user.username,
-          users: []
-        });
-      } catch (e) {
-          console.log(e.message);
-          res.status(401).render('pages/login', {
-              success: false,
-              message: e.message
-          });
+        if (suggestedUsers.length > 0) {
+          res.status(200).render('pages/home', {
+            username: user.username,
+            users: _.filter(suggestedUsers, (suggestedUser => {
+              return suggestedUser.username.localeCompare(user.username);
+            }))
+          })
+        } else {
+          res.status(200).render('pages/home', {
+            username: user.username,
+            users: []
+          })
         }
+      } catch (e) {
+        console.log(e.message);
+        res.status(401).render('pages/login', {
+          success: false,
+          message: e.message
+        });
+      }
     }
   })
 
