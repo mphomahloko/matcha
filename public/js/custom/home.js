d4658d0c6347be9c8fa538343f667950b0baab5b
KMashao <kmashao@users.noreply.github.com>
2020-07-13 00:50:28 +0200

    styling still wip

--- public/js/custom/home.js
+++ public/js/custom/home.js
@@ -1,1 +1,20 @@
 /*	HOME-PAGE	JS  */
+var rangeSlider = function(){
+  var slider = $('.range-slider'),
+      range = $('.range-slider__range'),
+      value = $('.range-slider__value');
+    
+  slider.each(function(){
+
+    value.each(function(){
+      var value = $(this).prev().attr('value');
+      $(this).html(value);
+    });
+
+    range.on('input', function(){
+      $(this).next(value).html(this.value);
+    });
+  });
+};
+
+rangeSlider();
