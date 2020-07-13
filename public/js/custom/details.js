5ef453fa2a490eb5af23fef8e85f00a48916d758
KMashao <kmashao@users.noreply.github.com>
2020-07-13 16:56:24 +0200

    no image shows on details page

--- public/js/custom/details.js
+++ public/js/custom/details.js
@@ -47,16 +47,3 @@ const dis_like = () => {
 
 // event listeners
 document.querySelector('button[dis-like]').addEventListener('click', dis_like);
-
-const
-    range = document.getElementById('range'),
-    rangeV = document.getElementById('rangeV'),
-    setValue = ()=>{
-        const
-            newValue = Number( (range.value - range.min) * 100 / (range.max - range.min) ),
-            newPosition = 10 - (newValue * 0.2);
-        rangeV.innerHTML = `<span>${range.value}</span>`;
-        rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`;
-    };
-document.addEventListener("DOMContentLoaded", setValue);
-range.addEventListener('input', setValue);
